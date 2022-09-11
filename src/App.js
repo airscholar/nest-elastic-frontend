d9879b1fd6c16a15a545ae570c13562afe019353
Yusuf Ganiyu <airscholar@gmail.com>
2022-09-11 22:17:09 +0100

    nest elastic frontend app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
+const App = () => {
+  const [searchResponse, setSearchResponse] = useState([]);
+  const [totalValue, setTotalValue] = useState();
 
-function App() {
+  const handleChange = async e => {
+    const { data } = await axios.post('http://localhost:8000/movies/search', {
+      data: {
+        title: e.target.value,
+      },
+    });
+
+    setSearchResponse(data.results);
+    setTotalValue(data.total.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container search-table'>
+        <div className='search-box'>
+          <div className='row'>
+            <div className='col-md-3'>
+              <h5>Search All Fields</h5>
+            </div>
+            <div className='col-md-9'>
+              <input
+                type='text'
+                id='myInput'
+                onChange={handleChange}
+                className='form-control'
+                placeholder='Search IMDB movies'></input>
+            </div>
+          </div>
+        </div>
+        <div className='search-list'>
+          <h3>
+            {totalValue ?? 0} {totalValue > 1 ? 'Records' : 'Record'} Found
+          </h3>
+          <table className='table' id='myTable'>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Overview</th>
+                <th>Revenue:Budget ($)</th>
+              </tr>
+            </thead>
+            <tbody>
+              {searchResponse.map((res, idx) => (
+                <tr key={idx}>
+                  <td className='title'>{res.title}</td>
+                  <td>
+                    <p>{res.overview}</p>
+                    <sub>"{res.tagline}"</sub>
+                  </td>
+                  <td>
+                    <p>
+                      <sub>
+                        {res.revenue.toLocaleString()}:{res.budget.toLocaleString()}
+                      </sub>
+                    </p>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
